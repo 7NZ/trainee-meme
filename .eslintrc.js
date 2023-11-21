92b5163423ac8b7395720c56527a5c17b440148d
7NZ <weiruan552406@live.com>
2023-11-21 10:27:56 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,15 +15,11 @@ module.exports = {
         }
     },
     "extends": [
-        "eslint:recommended"
+        "eslint:recommended",
+        "standard"
     ],
-    "ignorePatterns": ["*.json"],
+    "ignorePatterns": ["*.json", ".eslintrc.js"],
     "rules": {
-        "comma-spacing": ["error", { "before": false, "after": true }], // 强制逗号后面有空格
-        "quotes": ["warn", "single", { "avoidEscape": true }], // 使用单引号
-        "semi": ["error", "always", { "omitLastInOneLineBlock": false}], // 总是使用分号
-        "semi-spacing": "error", // 强制分号之前和之后使用一致的空格
-        "no-extra-semi": "error", // 禁止不必要的分号
         "no-undef": "error", // 禁止未声明的变量
         "no-unused-vars": "warn",
         "no-class-assign": "error", // 禁止修改类声明的变量
@@ -33,13 +29,29 @@ module.exports = {
         "no-mixed-spaces-and-tabs": "error", // 禁止使用 空格 和 tab 混合缩进
         "no-inner-declarations": "error", // 禁止在嵌套的块中出现变量声明或 function 声明
         "no-unreachable": "error", // 禁止在return、throw、continue 和 break 语句之后出现不可达代码
+        "no-await-in-loop": "error",
+        "no-return-await": "error",
+        "no-trailing-spaces": "error",
+        "no-unexpected-multiline": "error",
+        "max-statements-per-line": ["error", { "max": 1 }],
+        "semi": ["error", "always", { "omitLastInOneLineBlock": false}], // 总是使用分号
+        "semi-spacing": "error", // 强制分号之前和之后使用一致的空格
+        "comma-spacing": ["error", { "before": false, "after": true }], // 强制逗号后面有空格
+        "quotes": ["warn", "single", { "avoidEscape": true }], // 使用单引号
+        "indent": ["error", 2, { "SwitchCase": 1 }], // 缩进2
+        "no-extra-semi": "error", // 禁止不必要的分号
         "space-before-blocks": "error", // 强制块之前有空格
         "space-infix-ops": ["error", {"int32Hint": true}], // 要求操作符周围有空格
-        "indent": ["error", 2, { "SwitchCase": 1 }], // 缩进2
-        "no-mixed-spaces-and-tabs": "error", // 使用一致的缩进
+        "keyword-spacing": "error", // 关键字前后有空格，如if和else
+        "key-spacing": ["error", { "afterColon": true }], // 对象属性后有空格
+        "no-multiple-empty-lines": "error",
         "no-unexpected-multiline": "error",
-        "keyword-spacing": "error",
-        "key-spacing": ["error", { "afterColon": true }],
-        "arrow-spacing": "error"
+        "arrow-spacing": "error", // 箭头函数空格
+        "object-curly-newline": ["error", {
+            "ObjectExpression": { "multiline": true, "consistent": true, "minProperties": 3 },
+            "ObjectPattern": { "multiline": true, "consistent": true, "minProperties": 3 },
+            "ImportDeclaration": { "consistent": true },
+            "ExportDeclaration": { "multiline": true, "minProperties": 3 }
+        }]
     }
 }
